451b3083c50c962d1b5b40bc289d04aa06c596a5
r20125572-spec <r20125572@gmail.com>
2026-09-12 22:03:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ from telegram import TelegramObject
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # 🔑  BOT CREDENTIALS
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8653105443:AAHi9rpt9kSiF9vj_YdvxcmnYXrDCCzilbI")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8749456582:AAGXn9-1CSEYfbddCtLauTehXHOdcfkPj8I")
 OWNER_ID  = int(os.environ.get("OWNER_ID", "5502877086"))  # @lucifer2600
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
